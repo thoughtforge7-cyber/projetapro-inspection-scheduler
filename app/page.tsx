0aad27e97d7a838de4ed8ffa623e235043b7f9cb
thoughtforge7-cyber <thoughtforge7@gmail.com>
2026-02-22 18:32:51 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>ProjetaPro Inspection Scheduler</h1>
+      <p>System is running successfully.</p>
+    </main>
+  );
+}
